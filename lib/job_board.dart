88f93401d3a413688671abb774839b35d14a714b
developerSajib88 <developersajib88@gmail.com>
2023-07-31 09:24:26 +0600

    Second Commit

---
+++ lib/job_board.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class JobBoard extends StatefulWidget {
+  const JobBoard({super.key});
+
+  @override
+  State<JobBoard> createState() => _JobBoardState();
+}
+
+class _JobBoardState extends State<JobBoard> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
+
+
+class MyWidget extends StatefulWidget {
+  const MyWidget({super.key});
+
+  @override
+  State<MyWidget> createState() => _MyWidgetState();
+}
+
+class _MyWidgetState extends State<MyWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
