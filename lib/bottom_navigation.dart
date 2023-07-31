6b25349d07fcccadb15b01ba68d7a544eef77ede
developerSajib88 <developersajib88@gmail.com>
2023-07-31 09:28:24 +0600

    third commit

---
+++ lib/bottom_navigation.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MyBranch extends StatefulWidget {
+  const MyBranch({super.key});
+
+  @override
+  State<MyBranch> createState() => _MyBranchState();
+}
+
+class _MyBranchState extends State<MyBranch> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
